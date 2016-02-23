99bfed763d2754e0118bab47cb3204bed1a480ea
Andrey Galkin <andrey@futoin.org>
2016-02-24 01:44:35 +0200

    Changed cflocation and cflocationpool facts to generate 'default', if not set

--- lib/facter/cflocation.rb
+++ lib/facter/cflocation.rb
@@ -2,6 +2,8 @@ Facter.add('cf_location') do
     setcode do
         if File.exists? '/etc/cflocation'
             File.read('/etc/cflocation')
+        else
+            'default'
         end
     end 
 end
--- lib/facter/cfpool.rb
+++ lib/facter/cfpool.rb
@@ -2,6 +2,8 @@ Facter.add('cf_location_pool') do
     setcode do
         if File.exists? '/etc/cflocationpool'
             File.read('/etc/cflocationpool')
+        else
+            'default'
         end
     end 
 end
