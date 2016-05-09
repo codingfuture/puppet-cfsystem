92bc2901972ba1fe8258d1a43ad56c196627b1af
Andrey Galkin <andrey@futoin.org>
2016-05-09 17:41:49 +0300

    After puppet-lint --fix

--- manifests/dotenv.pp
+++ manifests/dotenv.pp
@@ -15,19 +15,19 @@ define cfsystem::dotenv(
     
     if !defined(File[$dotenv_file]) {
         file { $dotenv_file:
-            ensure => present,
+            ensure  => present,
             content => '',
             replace => false,
         }
     }
     
     file_line { "${dotenv_file}/${variable}":
-        ensure => present,
-        path => $dotenv_file,
-        line => "${variable}=\"${value}\"",
-        match => "^${variable}",
-        replace => true,
+        ensure   => present,
+        path     => $dotenv_file,
+        line     => "${variable}=\"${value}\"",
+        match    => "^${variable}",
+        replace  => true,
         multiple => true,
-        require => File[$dotenv_file],
+        require  => File[$dotenv_file],
     }
 }
